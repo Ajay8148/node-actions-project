19719f4fdddd97cc6c31db33cfbdd2b36eccd4d5
root <root@ip-172-31-6-122.ap-south-1.compute.internal>
2026-02-10 06:27:54 +0000

    Fix ESLint v9 config using CommonJS

---
+++ eslint.config.cjs
@@ -0,0 +1,21 @@
+const js = require('@eslint/js');
+const globals = require('globals');
+const prettier = require('eslint-config-prettier');
+
+module.exports = [
+  js.configs.recommended,
+  prettier,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest
+      }
+    },
+    rules: {
+      'no-unused-vars': 'warn',
+      'no-console': 'off'
+    }
+  }
+];
+
